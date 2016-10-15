6598d55c193dffdd462a3870a7fe72088432183e
DmytroM89 <dmitrom89@gmail.com>
2016-10-15 09:57:44 +0300

    add new models

---
+++ app/Models/Bookautors.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bookautors extends Model
+{
+    protected $table = 'bookautors';
+    protected $guarded = [''];
+}
